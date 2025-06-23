0168297c7d79abe64dc31f9853b8abd4a3a04d54
Aakriti Bhandari <aakritibhandari62@gmail.com>
2025-06-23 15:43:36 +0545

    schemas to enter and display the output of transaction and category is made

--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import date
+from model import Transaction_type, User
 
 class User_Create(BaseModel):
     username:str
@@ -12,7 +13,31 @@ class User_Read(BaseModel):
     email:EmailStr
     created_at:date
 
-    
+
+class Transaction_Create(BaseModel):
+    title:str
+    amount:int
+    type:Transaction_type
+    transaction_category_id:int
+    notes:str
+
+class Transaction_Read(BaseModel):
+    id:int
+    title:str
+    amount:int
+    type:Transaction_type
+    transaction_category_id:int
+    transaction_date:date
+
+
+class Category_Create(BaseModel):
+    category:str
+
+class Category_Read(BaseModel):
+    id:int
+    category:str
+
+
 class Token(BaseModel):
     access_token:str
     token_type:str
