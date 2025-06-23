d20f3fda42760a719697cfbd41d3d4c8e6893814
Aakriti Bhandari <aakritibhandari62@gmail.com>
2025-06-23 15:42:44 +0545

    sqlmodel made for transaction, transaction type, transaction category

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
-from sqlmodel import SQLModel, Field, ForeignKey
+from sqlmodel import SQLModel, Field, Relationship
 from pydantic import EmailStr
 from datetime import date
+from enum import Enum
+from typing import Optional, List
+
 
 class User(SQLModel,table=True):
     id:int|None=Field(primary_key=True, index=True)
@@ -8,3 +11,36 @@ class User(SQLModel,table=True):
     username:str=Field(index=True)
     email:EmailStr=Field( unique=True)
     password:str=Field()
+    transaction:List["Transaction"]=Relationship(back_populates="user")
+    category:List["Transaction_Category"]=Relationship(back_populates="user")
+
+class Transaction_Category(SQLModel,table=True):
+    id:int|None=Field(primary_key=True, index=True)
+    category:str|None=Field(index=True, unique=True)
+    transaction_link:List["Transaction"]=Relationship(back_populates="trans_category")
+    user_id:int=Field(foreign_key="user.id")
+    user:Optional[User]=Relationship(back_populates="category")
+
+
+class Transaction_type(str,Enum):
+    income="Income"
+    expense="Expense"
+
+
+class Transaction(SQLModel, table=True):
+    id : int|None=Field(primary_key=True, index=True)
+    user_id:int=Field(foreign_key="user.id")
+    user:Optional[User]=Relationship(back_populates="transaction")
+    title:str
+    amount:int
+    type:Transaction_type=Field(default=Transaction_type.expense,index=True)
+    transaction_category_id:Optional[int]=Field(foreign_key="transaction_category.id")
+    trans_category:Optional[Transaction_Category]=Relationship(back_populates="transaction_link")
+    transaction_date:date=Field(default_factory=date.today)
+    notes:int|None=Field(default=None)
+
+
+
+User.update_forward_refs()
+Transaction_Category.update_forward_refs()
+Transaction.update_forward_refs()
